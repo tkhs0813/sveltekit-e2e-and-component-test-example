d8955bb1d70589a2dda07dfc0cbcd4cf0af18fa6
tkhs0813 <takahashiry.0813@gmail.com>
2024-08-28 19:01:42 +0900

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run build -- --mode e2e && npm run preview -- --mode e2e',
 		port: 4173
 	},
 	testDir: 'tests',
--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,58 @@
 import { expect, test } from '@playwright/test';
+import { DockerComposeEnvironment, StartedDockerComposeEnvironment } from 'testcontainers';
+import type { IWireMockRequest, IWireMockResponse } from 'wiremock-captain';
+import { WireMock } from 'wiremock-captain';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	const title = await page.textContent('h2');
-	expect(title).toBe('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+test.describe('モック動作確認用テスト', () => {
+	let environment: StartedDockerComposeEnvironment;
+	let mock: WireMock;
+
+	test.beforeAll(async () => {
+		environment = await new DockerComposeEnvironment('./tests', 'compose.yml').up();
+		const wiremockEndpoint = 'http://localhost:3000';
+		mock = new WireMock(wiremockEndpoint);
+	});
+
+	test.afterAll(async () => {
+		await environment.down();
+	});
+
+	const request: IWireMockRequest = {
+		method: 'GET',
+		endpoint: '/posts/1'
+	};
+
+	test('check title 1', async ({ page }) => {
+		const mockedResponse: IWireMockResponse = {
+			status: 200,
+			body: {
+				userId: 1,
+				id: 1,
+				title: 'test title',
+				body: 'test body'
+			}
+		};
+		await mock.register(request, mockedResponse);
+
+		await page.goto('/');
+		const title = await page.textContent('h2');
+		expect(title).toBe('test title');
+	});
+
+	test('check title 2', async ({ page }) => {
+		const mockedResponse: IWireMockResponse = {
+			status: 200,
+			body: {
+				userId: 1,
+				id: 1,
+				title: 'test title2',
+				body: 'test body2'
+			}
+		};
+		await mock.register(request, mockedResponse);
+
+		await page.goto('/');
+		const title = await page.textContent('h2');
+		expect(title).toBe('test title2');
+	});
 });
